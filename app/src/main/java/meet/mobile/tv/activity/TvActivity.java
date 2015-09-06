599082085a112b1c56adecc6b80e907198c6afda
Filip Zymek <filip.zymek@gmail.com>
2015-09-06 21:37:47 +0200

    Add TV & Leanback dependencies and Simple TV Activity

---
+++ app/src/main/java/meet/mobile/tv/activity/TvActivity.java
@@ -0,0 +1,18 @@
+package meet.mobile.tv.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import meet.mobile.R;
+
+/**
+ * Created by Filip on 2015-09-06.
+ */
+public class TvActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tv);
+	}
+}
